d5c8ae1e16f768ff65dea2092dbf655623497fcf
danielle <danielle@checkpoint.com>
2019-06-20 13:37:51 +0300

    Peace and Love Hare Krishna

--- V1.py
+++ V1.py
@@ -1,6 +1,8 @@
 #Eyatar Nezer
 #Daniel Levy
 
+import re
+
 filePath = "C:\\Users\\danielle\\Desktop\\vpnd.elg"
 file = None
 
@@ -9,4 +11,8 @@ print("Program started")
 def open_file(file, filePath, mode):
     file = open(filePath, mode)
 
-open_file(file, filePath, "a")
+    line = file.readline()
+    pid = re.search("\s*\d{3,}\s", line, flags=0).group()
+    print("PID:" +  pid)
+
+open_file(file, filePath, "r")
